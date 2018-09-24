354b403274ab5503fbe2ca16981686dbc18b3d78
harbaja16 <honzah70@gmail.com>
2018-09-24 22:41:36 +0200

    draft2

--- Battleship/Battleship/Program.cs
+++ Battleship/Battleship/Program.cs
@@ -16,67 +16,18 @@ namespace Battleship
             TileMap Map1 = new TileMap(size);
 
             Ship ship = new Ship(ShipClass.Hydroplane);
+            Ship ship2 = new Ship(ShipClass.Carrier);
 
             Map1.AddShip(ship, 55);
+            Map1.AddShip(ship2, 54);
 
-            int index;
-            for (int i = 0; i < Map1.Size.height; i++)
-            {
-                for (int z = 0; z < Map1.Size.width; z++)
-                {
-                    index = Map1.Size.width * i + z;
-                    if(Map1.Tiles[index].Ship != null)
-                    {
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGray;
-                    }
-                    Console.Write("■ ");
-                    Console.ResetColor();
-                }
-                Console.WriteLine();
-            }
+            Map1.DisplayMap();
             Console.WriteLine();
 
             ship.Rotate();
 
-            for (int i = 0; i < Map1.Size.height; i++)
-            {
-                for (int z = 0; z < Map1.Size.width; z++)
-                {
-                    index = Map1.Size.width * i + z;
-                    if (Map1.Tiles[index].Ship != null)
-                    {
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGray;
-                    }
-                    Console.Write("■ ");
-                    Console.ResetColor();
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            /*
-            index = 0;
-            foreach (var item in Map1.Tiles)
-            {
-                if (item.Ship == null)
-                {
-                    Console.WriteLine("{0}: 0", index);
-                }
-                else
-                {
-                    Console.WriteLine("{0}: 1 ", index);
-                }
-                index++;
-            }
-            Console.WriteLine();
-            */
+            Map1.DisplayMap();
+
             Console.Read();
         }
     }
--- Battleship/Battleship/TileMap.cs
+++ Battleship/Battleship/TileMap.cs
@@ -24,17 +24,7 @@ namespace Battleship
                 }
             }
 
-            foreach (var point in points)
-            {
-                for (int i = 0; i < Tiles.Count; i++)
-                {
-                    if(Tiles[i].Position.X == point.X && Tiles[i].Position.Y == point.Y)
-                    {
-                        Tiles[i].Ship = ship;
-                    }
-                }
-            }
-            
+            Tiles[index].Ship = ship;
             return true;
         }
 
@@ -42,6 +32,76 @@ namespace Battleship
         {
             Size = mapSize;
             createMap();
+        }
+
+        public void DisplayMap()
+        {
+            var query = from tile in Tiles
+                        where tile.Ship != null
+                        select tile;
+
+            List<Point> pointList = new List<Point>();
+            foreach (var tile in query)
+            {
+                pointList.AddRange(ShipFunctions.GetPoints(tile.Ship, tile.Position));
+            }
+
+            List<Point> overflowedPositions = new List<Point>();
+
+            for (int i = 0; i < pointList.Count - 1; i++)
+            {
+                for (int z = 1 + i; z < pointList.Count; z++)
+                {
+                    if (pointList[i].X == pointList[z].X && pointList[i].Y == pointList[z].Y)
+                    {
+                        overflowedPositions.Add(pointList[i]);
+                    }
+                }
+            }
+
+            int index;
+            for (int i = 0; i < Size.height; i++)
+            {
+                for (int z = 0; z < Size.width; z++)
+                {
+                    index = Size.width * i + z;
+                    Point position = Tiles[index].Position;
+                    int pointType = 0;
+
+                    foreach (var point in overflowedPositions)
+                    {
+                        if(position.X == point.X && position.Y == point.Y)
+                        {
+                            pointType = 2;
+                            break;
+                        }
+                    }
+
+                    if (pointType == 0)
+                    {
+                        foreach (var point in pointList)
+                        {
+                            if (position.X == point.X && position.Y == point.Y)
+                            {
+                                pointType = 1;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (pointType == 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else if(pointType == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.Write("■ ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
         }
 
         private void createMap()
